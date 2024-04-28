9baae53fb48f99156107f6411e58dfc3750354c7
shreeshvyas <shreeshvyas2@gmail.com>
2024-04-28 14:31:13 +0530

    fixed domains

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.configure do
+  config.hosts << "docker-app-a82l.onrender.com"
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
